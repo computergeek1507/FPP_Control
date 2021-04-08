4a573cab26e13657ad641a00392280a0baccdb52
Scott Hanson <scooter_seh@yahoo.com>
2021-04-08 00:29:37 -0400

    Added Playlist Start/Stop Support, Added Start/Stop All testing to menu, remember all IP address until manually cleared

--- app/src/main/java/com/example/fpptest/MainActivity.java
+++ app/src/main/java/com/example/fpptest/MainActivity.java
@@ -27,12 +27,16 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.android.volley.AuthFailureError;
+import com.android.volley.VolleyLog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences pref;
     ArrayAdapter<FPPData> array_Adapter;
     RequestQueue queue;
+    List<String> ip_list = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -73,11 +79,20 @@ public class MainActivity extends AppCompatActivity {
                         FPPData fpp = array_Adapter.getItem(position);
 
                         switch (item.getItemId()) {
+                            case R.id.startplaylist:
+                                GetPlaylists(fpp);
+                                return true;
+                            case R.id.stopplaylist:
+                                StopPlaylistGracefully(fpp);
+                                return true;
+                            case R.id.stopplaylistnow:
+                                StopPlaylistNow(fpp);
+                                return true;
                             case R.id.starttest:
-                                SetTesting(fpp.getIP(),1);
+                                SetTesting(fpp,1);
                                 return true;
                             case R.id.stoptest:
-                                SetTesting(fpp.getIP(),0);
+                                SetTesting(fpp,0);
                                 return true;
                             case R.id.open:
                                 Uri uriUrl = Uri.parse("http://" + fpp.getIP());
@@ -94,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         LoadFPPList();
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -108,9 +124,24 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_add:
                 Show_Input();
                 return true;
-            case R.id.action_main_ip:
-                Show_Main_Input();
+            case R.id.action_start_all_test:
+                for (FPPData fpp : fpp_list)
+                {
+                    SetTesting(fpp,1);
+                }
                 return true;
+            case R.id.action_stop_all_test:
+                for (FPPData fpp : fpp_list)
+                {
+                    SetTesting(fpp,0);
+                }
+                return true;
+            case R.id.action_clear_ips:
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("Prev_IPs", "");
+                editor.apply();
+                return true;
+
             //case R.id.action_settings:
                //showHelp();
              //   return true;
@@ -132,37 +163,6 @@ public class MainActivity extends AppCompatActivity {
         alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 Probe_FPP(input.getText().toString().trim() ,true);
-            }
-        });
-
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                Toast.makeText(getApplicationContext(), "User Cancelled", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        alert.show();
-    }
-
-    void Show_Main_Input()
-    {
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle("Set Main FPP Device");
-        alert.setMessage("Enter Host Name or IP Address:");
-
-        // Set an EditText view to get user input
-        final EditText input = new EditText(this);
-        alert.setView(input);
-
-        input.setText(pref.getString("Main_IP", ""));
-
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String mainIP = input.getText().toString().trim();
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("Main_IP", mainIP);
-                editor.apply();
-                Probe_FPP(mainIP ,true);
             }
         });
 
@@ -198,6 +198,11 @@ public class MainActivity extends AppCompatActivity {
         FPPData fpp = new FPPData(json);
         if(!fpp_list.contains(fpp)) {
             fpp_list.add(fpp);
+
+            if(!ip_list.contains(fpp.getIP())) {
+                ip_list.add(fpp.getIP());
+            }
+
             Collections.sort(fpp_list, new FPPDataComparator());
             array_Adapter.notifyDataSetChanged();
 
@@ -218,6 +223,11 @@ public class MainActivity extends AppCompatActivity {
                 if(!fpp_list.contains(fpp)) {
                     fpp_list.add(fpp);
                 }
+
+                if(!ip_list.contains(fpp.getIP())) {
+                    ip_list.add(fpp.getIP());
+                }
+
             }
             Collections.sort(fpp_list, new FPPDataComparator());
 
@@ -225,7 +235,7 @@ public class MainActivity extends AppCompatActivity {
             SaveFPPList();
         }
         catch (JSONException ex) {
-
+            Toast.makeText(getApplicationContext(), "That didn't work!\n" + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
     
@@ -249,11 +259,6 @@ public class MainActivity extends AppCompatActivity {
 
     void SaveFPPList()
     {
-        List<String> ip_list = new ArrayList<String>();
-
-        for (FPPData fpp : fpp_list) {
-            ip_list.add(fpp.getIP());
-        }
         String ips = TextUtils.join(",", ip_list);
         SharedPreferences.Editor editor = pref.edit();
         editor.putString("Prev_IPs", ips);
@@ -262,11 +267,6 @@ public class MainActivity extends AppCompatActivity {
 
     void LoadFPPList()
     {
-        String main_ip = pref.getString("Main_IP", "");
-        if(!main_ip.isEmpty()) {
-            Probe_FPP(main_ip, true);
-        }
-
         String ips = pref.getString("Prev_IPs", "");
         String[] arrOfIps = ips.split(",");
 
@@ -279,7 +279,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void SetTesting(String ip, int enable)
+    void SetTesting(FPPData fpp, int enable)
     {
         try {
             JSONObject testSet = new JSONObject();
@@ -294,29 +294,223 @@ public class MainActivity extends AppCompatActivity {
 
             String data = testSet.toString();
 
-            String url = String.format("http://%s/fppjson.php", ip);
-            StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Toast.makeText(getApplicationContext(), "Sent Successfully" , Toast.LENGTH_SHORT).show();
-                }
-            }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(), "Error!\n" + error.getMessage() , Toast.LENGTH_LONG).show();
-                }
-            }) {
-                protected Map<String, String> getParams() {
-                    Map<String, String> MyData = new HashMap<String, String>();
-                    MyData.put("command", "setTestMode");
-                    MyData.put("data", data);
-                    return MyData;
-                }
-            };
-            queue.add(MyStringRequest);
+            if(fpp.getVerMajor() > 4)
+            {
+                String url = String.format("http://%s/api/testmode", fpp.getIP());
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Sent Successfully" , Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error!\n" + error.getMessage() , Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8";
+                    }
+
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        try {
+                            return data == null ? null : data.getBytes("utf-8");
+                        } catch (UnsupportedEncodingException uee) {
+                            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", data, "utf-8");
+                            return null;
+                        }
+                    }
+                };
+                queue.add(MyStringRequest);
+            }
+            else
+            {
+                String url = String.format("http://%s/fppjson.php", fpp.getIP());
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Sent Successfully" , Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error!\n" + error.getMessage() , Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> MyData = new HashMap<String, String>();
+                        MyData.put("command", "setTestMode");
+                        MyData.put("data", data);
+                        return MyData;
+                    }
+                };
+                queue.add(MyStringRequest);
+            }
         }
         catch (Exception ex)
         {
+            Toast.makeText(getApplicationContext(), "That didn't work!\n" + ex.getMessage(), Toast.LENGTH_LONG).show();
         }
     }
+
+    private void GetPlaylists(FPPData fpp) {
+        //http://192.168.1.5/api/playlists
+
+        String url=String.format("http://%s/api/playlists", fpp.getIP());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        SelectPlaylist(fpp, response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "That didn't work!\n" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void SelectPlaylist(FPPData fpp, String json)
+    {
+        try{
+            List<String> playLists = new ArrayList<String>();
+            JSONArray reader = new JSONArray(json);
+            for(int i = 0; i <reader.length(); ++i) {
+                playLists.add(reader.getString(i));
+            }
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Select Playlist")
+                    .setItems(playLists.toArray(new String[0]), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // The 'which' argument contains the index position
+                            // of the selected item
+                            StartPlaylist(fpp, playLists.get(which));
+                        }
+                    });
+            builder.show();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(getApplicationContext(), "That didn't work!\n" + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void StartPlaylist(FPPData fpp, String playlist) {
+        try{
+            if(fpp.getVerMajor() > 4){
+                JSONObject testSet = new JSONObject();
+                testSet.put("command", "Start Playlist");
+
+                JSONArray array = new JSONArray();
+                array.put(playlist);
+                array.put("false");
+                array.put("false");
+                testSet.put("args", array);
+
+                String data = testSet.toString();
+
+                String url=String.format("http://%s/api/command", fpp.getIP());
+
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Sent Successfully" , Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Error!\n" + error.getMessage() , Toast.LENGTH_LONG).show();
+                    }
+                }) {
+                    @Override
+                    public String getBodyContentType() {
+                        return "application/json; charset=utf-8";
+                    }
+
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        try {
+                            return data == null ? null : data.getBytes("utf-8");
+                        } catch (UnsupportedEncodingException uee) {
+                            VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", data, "utf-8");
+                            return null;
+                        }
+                    }
+                };
+                queue.add(MyStringRequest);
+            }
+            else
+            {
+                String url=String.format("http://%s/fppxml.php?command=startPlaylist&playList=%s&repeat=0", fpp.getIP(), playlist);
+                //fppxml.php?command=startPlaylist&playList=" + Playlist + "&repeat=" + repeat + "&playEntry=" + PlayEntrySelected + "&section=
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(getApplicationContext(), "Sent Successfully" , Toast.LENGTH_SHORT).show();
+                                //add_fpp_device(response, probeForOthers);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "That didn't work!\n" + error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+                queue.add(stringRequest);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(getApplicationContext(), "That didn't work!\n" + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void StopPlaylistNow(FPPData fpp) {
+        ///api/sequence/current/stop
+
+        String url=String.format("http://%s/fppxml.php?command=stopNow", fpp.getIP());
+        if(fpp.getVerMajor() > 4){
+            url = String.format("http://%s/api/playlists/stop", fpp.getIP());
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //add_fpp_device(response, probeForOthers);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "That didn't work!\n" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
+    private void StopPlaylistGracefully(FPPData fpp) {
+        String url=String.format("http://%s/fppxml.php?command=stopGracefully", fpp.getIP());
+        if(fpp.getVerMajor() > 4){
+            url = String.format("http://%s/api/playlists/stopgracefully", fpp.getIP());
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //add_fpp_device(response, probeForOthers);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "That didn't work!\n" + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
+    }
+
 }
